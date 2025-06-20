7c5dccde1de7bcd8832f76092ec4492b6bb20613
ccebh <curecompany>
2025-06-20 13:34:08 +0200

    add TypeConverter that converts an Integer to BigDecimal

---
+++ base/src/main/java/com/blazebit/expression/base/converter/IntegerToBigDecimalTypeConverter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 - 2025 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.expression.base.converter;
+
+import com.blazebit.domain.runtime.model.DomainType;
+import com.blazebit.expression.ExpressionInterpreter;
+import com.blazebit.expression.spi.TypeConverter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author Harald Eibensteiner
+ * @since 1.0.0
+ */
+public class IntegerToBigDecimalTypeConverter implements TypeConverter<Integer, BigDecimal>, Serializable {
+    @Override
+    public BigDecimal convert(ExpressionInterpreter.Context context, Integer value, DomainType domainType) {
+        return new BigDecimal(value);
+    }
+}
